ae794204599d3250b929afe3b0e44854ed1b991e
William Brady <will@crofton.cloud>
2025-01-07 16:55:19 -0500

    Add docstrings

--- modules/jira_module.py
+++ modules/jira_module.py
@@ -5,6 +5,17 @@ import logging
 logger = logging.getLogger(__name__)
 
 def verify_jira_connection(url, user, token):
+    '''
+    Verify that the Jira connection is successful.
+
+    Args:
+        url (str): The Jira base URL.
+        user (str): The Jira username.
+        token (str): The Jira
+
+    Returns:
+        None
+    '''
     try:
         response = requests.get(
             f"{url}/rest/api/3/myself",
